4fda7fe51160f05a3ed1d8764127300e2a902100
CodeNinja0615 <sameerakhtar1513@gmail.com>
2024-11-30 01:15:15 +0530

    Added iOS, Windows and Mac automation placeholders

--- src/test/java/sameerakhtar/TestComponents/BaseTest.java
+++ src/test/java/sameerakhtar/TestComponents/BaseTest.java
@@ -21,25 +21,27 @@ import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
 import io.appium.java_client.service.local.AppiumDriverLocalService;
 import io.appium.java_client.service.local.AppiumServiceBuilder;
+import io.appium.java_client.windows.WindowsDriver;
+import io.appium.java_client.windows.options.WindowsOptions;
 import sameerakhtar.pageobject.LaunchGameWithPackageName;
 
 public class BaseTest {
 
 	public AppiumDriverLocalService service;
 	public AndroidDriver driver;
+	public WindowsDriver winDriver;
 	public LaunchGameWithPackageName launchGameWithPackageName;
 
 	public void configureAppium(String deviceName, String platformName, boolean setNoReset)
 			throws MalformedURLException, URISyntaxException {
-
+		String currentUser = System.getProperty("user.name");
+		service = new AppiumServiceBuilder()
+				.withAppiumJS(new File("C://Users//" + currentUser
+						+ "//AppData//Roaming//npm//node_modules//appium//build//lib//main.js"))
+				.withIPAddress("127.0.0.1").usingPort(4723).build();
+		service.start();
 		if (platformName.equalsIgnoreCase("Android")) {
-
-			String currentUser = System.getProperty("user.name");
-			service = new AppiumServiceBuilder()
-					.withAppiumJS(new File("C://Users//" + currentUser
-							+ "//AppData//Roaming//npm//node_modules//appium//build//lib//main.js"))
-					.withIPAddress("127.0.0.1").usingPort(4723).build();
-			service.start();
+			// ---Android code here
 			UiAutomator2Options options = new UiAutomator2Options();
 			options.setDeviceName(deviceName);
 			options.setPlatformName(platformName);
@@ -56,15 +58,30 @@ public class BaseTest {
 			driver = new AndroidDriver(new URI("http://127.0.0.1:4723").toURL(), options);
 			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 			driver.unlockDevice();
+		} else if (platformName.equalsIgnoreCase("iOS")) {
+			// ---iOS code here
 		}
-		else if(platformName.equalsIgnoreCase("iOS")) {
-			//---iOS code here
-		}
+	}
 
-		else if(platformName.equalsIgnoreCase("windows")) {
-			//---windows code here
+	public void configureAppiumWindows(String deviceName, String platformName, String appToLaunch)
+			throws MalformedURLException, URISyntaxException {
+		String currentUser = System.getProperty("user.name");
+		service = new AppiumServiceBuilder()
+				.withAppiumJS(new File("C://Users//" + currentUser
+						+ "//AppData//Roaming//npm//node_modules//appium//build//lib//main.js"))
+				.withIPAddress("127.0.0.1").usingPort(4723).build();
+		service.start();
+		if (platformName.equalsIgnoreCase("Windows")) {
+			// ---Windows code here
+			WindowsOptions options = new WindowsOptions();
+			options.setCapability("deviceName", deviceName);
+			options.setCapability("platformName", platformName);
+			// ----CMD---- // Get-StartApps
+			options.setCapability("app", appToLaunch);
+			winDriver = new WindowsDriver(new URI("http://127.0.0.1:4723").toURL(), options);
+		} else if (platformName.equalsIgnoreCase("Mac")) {
+			// ---Mac code here
 		}
-
 	}
 
 	@BeforeMethod
@@ -78,9 +95,13 @@ public class BaseTest {
 		String platformName = System.getProperty("platformName") != null ? System.getProperty("platformName")
 				: prop.getProperty("platformName");
 
-		configureAppium(deviceName, platformName, true);
-		launchGameWithPackageName = new LaunchGameWithPackageName(driver); // ----Providing driver to the first page
-																			// object model class
+		if (platformName.equalsIgnoreCase("Android") || platformName.equalsIgnoreCase("iOS")) {
+			configureAppium(deviceName, platformName, true);
+			launchGameWithPackageName = new LaunchGameWithPackageName(driver); // ----Providing driver to the first page
+																				// object model class
+		} else if (platformName.equalsIgnoreCase("Windows") || platformName.equalsIgnoreCase("Mac")) {
+			configureAppiumWindows("WindowsPC", "Windows", "Microsoft.GamingApp_8wekyb3d8bbwe!Microsoft.Xbox.App");
+		}
 	}
 
 	@AfterMethod
